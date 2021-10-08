a99c30636ab9b3762d5c5f4a883d42afda039a61
Swathi146 <92150543+Swathi146@users.noreply.github.com>
2021-10-08 14:08:36 +0530

    Add files via upload

---
+++ Gold.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int side,area;
+	area=side*side;
+	printf("%d",area);
+
+}
---
+++ doll.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=20,b;
+	b=a++;
+	printf("%d\n",a);
+	printf("%d",b);
+}
