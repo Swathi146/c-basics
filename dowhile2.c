5a5484e76bfb02c6a83ecb32945568b6aeda11cb
Swathi146 <92150543+Swathi146@users.noreply.github.com>
2021-10-29 14:26:43 +0530

    Add files via upload

---
+++ dowhile2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int  main()
+{
+	int a,b,sum;
+	scanf("%d%d",&a,&b);
+	sum=sub(a,b);
+	printf("%d",sum);
+}
+    int sub(int a,int b)
+    {
+    	int c;
+    	c=a-b;
+    	return c;
+}
