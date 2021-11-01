9d76193016c12aecf241cf9445db2287dd9f696a
Swathi146 <92150543+Swathi146@users.noreply.github.com>
2021-11-01 15:16:29 +0530

    Add files via upload

---
+++ isprimefunction.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int isprime(int);
+int main()
+{
+	int n,i;
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    if(isprime(i))
+    {
+    	printf("%d\n",i);
+	}
+}
+int isprime(int n)
+{
+	int i;
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
